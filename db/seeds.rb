bed375ab76dfc8feb6121d8431df0cd70fd0f9b4
尹常鑫 <ycxxww@gmail.com>
2013-08-15 22:42:25 +0800

    修改初始化数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,25 @@
-# Admin.create! do |user|
-#   user.password = "password"
-#   user.password_confirmation = "password"
-#   user.email = "admin@localhost.local"
-#   user.skip_captcha = true
-#   user.confirmed_at = DateTime.now
-# end
+#encoding : utf-8
+Admin.create! do |user|
+  user.password = "bitwinadmin"
+  user.password_confirmation = "bitwinadmin"
+  user.email = "admin@bitwin.co"
+  user.skip_captcha = true
+  user.confirmed_at = DateTime.now
+end
 
 # puts "Created \"admin@localhost.local\" user with password \"password\""
-#encoding : utf-8
 
 operation = Operation.create
 %w{test1@bitwin.co test2@bitwin.co test3@bitwin.co}.each do |email|
+	User.create! do |user|
+		user.password = "testtest"
+		user.password_confirmation = "testtest"
+		user.email = email
+		user.confirmed_at = DateTime.now
+	end
   AccountOperation::CURRENCIES.each do |currency|
     ap = AccountOperation.create({
-                  amount: 100000000,
+                  amount: 100000,
                   currency: currency
       })
     ap.account = User.find_by_email(email)
